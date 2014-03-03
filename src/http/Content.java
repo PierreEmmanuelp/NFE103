4899a43650ba53020c8cdea5f447f4d0ec05538a
hervetruc <truchardherve@gmail.fr>
2014-03-03 13:50:36 +0100

    Mise à jour classe content

--- src/http/Content.java
+++ src/http/Content.java
@@ -3,53 +3,16 @@ package http;
 import java.io.*;
 
 /**
- *
+ *récupère contenur requete
  * @author Pierre-Emmanuel Pourquier,Herve Truchard
  * @version 1.0
+ * @return void
  */
 public class Content {
     
     private String contenu;
-    private String CheminCible;
     
-
-   public void openFile (String CheminCible) {
-        this.CheminCible = CheminCible;
-        readFile();
-
-}
-   
-   /*enregistrement d un fichier dans une variable de classe*/
-    private String readFile (){
-        
-   
-     
-     String myLine="";
-     
-		try{
-			//création des flux
-			InputStreamReader in=new InputStreamReader(new FileInputStream(this.CheminCible));
-			
-				 LineNumberReader llog = new LineNumberReader(in);
-				//copie du fichier
-				
-					while ((myLine = llog.readLine()) != null) 
-                                        { 
-                      // --- Ajout de la ligne au contenu
-                                        contenu += myLine;
-                                         }
-                                        
-					//forcer l'ecriture du contenu du tampon dans le fichier
-					
-                                        System.out.println("le contenu" +contenu );
-                                       
-                                    }catch(Exception e){
-                                
-                            }
-          return contenu;      
-    }
-
-
+    
     public String getContenu() {
         return contenu;
     }
@@ -58,12 +21,6 @@ public class Content {
         this.contenu = contenu;
     }
 
-    public String getCheminCible() {
-        return CheminCible;
-    }
-
-    public void setCheminCible(String CheminCible) {
-        this.CheminCible = CheminCible;
-    }
+   
    
 }
