6d7d29c3d8c61b2dc0fccd57e7f18d7d0c958530
morgan <morgan@vmxubuntu>
2014-03-04 20:32:37 +0100

    création de la classe Host : représente un hôte avec un nom et un chemin

--- src/http/Host.java
+++ src/http/Host.java
@@ -7,4 +7,53 @@ package http;
  */
 public class Host {
     
+    private String name;
+    private String path;
+    
+    /**
+    * contructeur de la classe. 
+    * @param pName 
+    * @param pPath
+    */
+    public Host(String pName, String pPath){
+        this.name = pName;
+        this.path = pPath;
+    }
+    
+    /**
+    * permet de récupérer le nom de l'hôte
+    * @return le nom de l'hôte
+    */
+    public String getName() 
+    {
+        return name;
+    }
+    
+     /**
+    * permet de récupérer le chemin de l'hôte
+    * @return le chemin de l'hôte
+    */
+    public String getPath() 
+    {
+        return path;
+    }
+    
+     /**
+    * permet de définir le nom de l'hôte
+    * @param pName
+    */
+    public void setName(String pName)
+    {
+        this.name=pName;
+    }
+    
+     /**
+    * permet de définir le chemin de l'hôte
+    * @param pPath
+    */
+    public void setPath(String pPath)
+    {
+        this.path=pPath;
+    }
+    
 }
