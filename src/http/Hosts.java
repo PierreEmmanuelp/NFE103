666cdfc4fb9b2e4cab555662a10c65f324e1789a
morgan <morgan@vmxubuntu>
2014-03-04 20:31:26 +0100

    création de la classe Hosts : collection d'objets Host permet de récupérer le chemin d'un hôte en fonction de son nom

---
+++ src/http/Hosts.java
@@ -0,0 +1,52 @@
+package http;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
+/**
+ * @author morgan
+ * @version 1.0
+ */
+public class Hosts {
+    
+    private final ArrayList<Host> hostList;
+
+    /**
+    * contructeur de la classe
+    */
+    public Hosts(){
+        this.hostList = new ArrayList();
+    }
+    
+    public void addHost(Host pHost){
+         this.hostList.add(pHost);
+    }
+    
+     /**
+    * permet de récupérer le nom de l'hôte
+    * @param pName
+    * @return le nom de l'hôte
+    */
+    public String getPath(String pName){
+        Iterator<Host> i = hostList.iterator();		
+        while(i.hasNext())
+        {
+            Host host = (Host)i.next();
+            if(host.getName().matches(pName)){
+                return host.getPath();
+            }
+       }
+       return null;
+    }
+    
+    /**
+    * permet de récupérer la liste des hôtes
+    * @return la liste des hôtes
+    */
+    public List<Host> getHostList() 
+    {
+        return hostList;
+    }
+    
+}
