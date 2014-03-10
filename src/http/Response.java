a15b784119b0e3108933092a2af4e12246240283
pierrot <benjamin.pierrot@univ-st-etienne.fr>
2014-03-10 17:18:26 +0100

    Préparation au formatage du header (version incomplète)

--- src/http/Response.java
+++ src/http/Response.java
@@ -1,4 +1,6 @@
 package http;
+import java.util.Date;
+import java.util.*;
 
 /**
  * Correspond à un réponse HTTP
@@ -31,10 +33,10 @@ public class Response {
      * CRLF
      */
     public final static String CRLF = "\r\n";
-//    /**
-//     * Protocol.
-//     */
-//    public final static String PROTOCOL = "HTTP/1.0 ";
+    /**
+     * Protocol.
+     */
+    public final static String PROTOCOL = "HTTP/1.0 ";
     /**
      * All OK.
      */
@@ -55,15 +57,47 @@ public class Response {
      * Resource internal system error.
      */
     private final static String INTERNAL_ERROR = "500 Internal Server Error";
+    /**
+    * Current header.
+    */
+    protected Hashtable myHeaders = new Hashtable();
+   
     
+    /**
+     * Todo : construction du header + response 
+     * @return 
+     */
 
+    private String headerRep() {
+                        
+     Date date = new Date();
+         
+      //this.myHeaders.put("Status","HTTP/1.0 200 OK");
+      //this.myHeaders.put("200","OK");
+      this.myHeaders.put("Date",date); 
+      this.myHeaders.put("Server","CNAM_NFE103/1.0"); // see param
+      this.myHeaders.put("Content-Type","text/HTML"); // Mime type
+      this.myHeaders.put("Content-Length","1234"); // length of chain
+      
+      String line = this.PROTOCOL + this.OK;
+      String key;
+      Enumeration e = this.myHeaders.keys();
+      while(e.hasMoreElements())
+      {
+         key = (String)e.nextElement();
+         line += key + ": " + this.myHeaders.get(key) + CRLF;
+      }
+      line += CRLF;
+      return line;
+      
+    }  
+    
     public String genereResponse(String request) {
         
-        return request.toString();
+        return this.headerRep() + request.toString();
     }
-    
+ 
     public String genererErreur500() {
         return this.genereResponse(INTERNAL_ERROR);
     }
-
 }
