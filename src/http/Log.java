6d50a020d902e4d7af71b7a4fc778c2d5da50284
Pierre-Emmanuel.Pour <Pierre-Emmanuel.Pour@HMVAFN83.suedwesteuropa.rheinzink.int>
2014-03-18 14:28:42 +0100

    Règles d'écriture java

--- src/http/Log.java
+++ src/http/Log.java
@@ -2,77 +2,78 @@
  * Fichier de log de l'application
  */
 package http;
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 
-/**
- * Représente un fichier de log 
+/**.
+ * Représente un fichier de log
  * @author Pierre-Emmanuel.Pourquier
  * @version 1.1
  */
 public class Log {
-        static String path="/var/log/http/";
-	
-	
-	/**
-	 * initialise le buffer de sortie dans le fichier
-	 * @throws IOException 
-	 */
-	public Log() throws IOException {
-               path = http.Http.config.getPathLog();
-	}
+    /**.
+     * chemin du dossier contenant les fichier de log
+     */
+    private static String path = "/var/log/http/";
+
+    /**.
+    * initialise le buffer de sortie dans le fichier
+    * @throws IOException concernant le filesystem
+    */
+    public Log() throws IOException {
+        path = http.Http.getConfig().getPathLog();
+    }
 
 
-	/**
-	 * renvoit le chemin du fichier de log
-	 * @return the path
-	 */
-	public String getPATH() {
-		return path;
-	}
-	
-	
-	/**
-	 * Ajoute un ligne à la fin du fichier de log
-	 * @param ligneLog correspond 
-	 * @throws IOException 
-	 */
-	synchronized static void ajouterEntree(String pligneLog,LogLevel plog){
-		FileWriter fw;
-		try {
-			fw = new FileWriter (path+plog+".log",true);
-			BufferedWriter bw = new BufferedWriter (fw);
-                        PrintWriter fichier = new PrintWriter (bw);
-                        String format = "dd/MM/yy H:mm:ss"; 
-                        java.text.SimpleDateFormat formater = new java.text.SimpleDateFormat( format ); 
-                        java.util.Date date = new java.util.Date(); 
-                        fichier.println(formater.format(date)+" : "+pligneLog);
-                        fichier.close();
-                         
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.println("erreur log");
-		}
-               
+    /**.
+    * renvoit le chemin du fichier de log
+    * @return the path
+    */
+    public static String getPath() {
+        return path;
+    }
 
-	}
-	
-	/**
-	 * 
-	 */
-	synchronized static void terminerSessionLog(){
-		FileWriter fw;
-		try {
-			fw = new FileWriter (path,true);
-			BufferedWriter bw = new BufferedWriter (fw);
-                        PrintWriter fichier = new PrintWriter (bw);
-                        fichier.println("\n\n");      
-		} catch (IOException e) {
-			e.getMessage();
-		}
+    /**.
+    * Ajoute un ligne à la fin du fichier de log
+    * @param pligne correspond à la ligne à écrire
+    * @param pLvl correspond au niveau du fichier à écrire
+    */
+    static synchronized void ajouterEntree(final String pligne, final LogLevel pLvl) {
+        FileWriter fw;
+        try {
+            fw = new FileWriter(path + pLvl + ".log", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter fichier;
+            fichier = new PrintWriter(bw);
+            String format = "dd/MM/yy H:mm:ss";
+            SimpleDateFormat formater;
+            formater = new SimpleDateFormat(format);
+            java.util.Date date;
+            date = new java.util.Date();
+            fichier.println(formater.format(date) + " : " + pligne);
+            fichier.close();
+        } catch (IOException e) {
+            debug.Trace.trace(e.getMessage());
+            System.out.println("erreur dans les fichiers de log");
+        }
+    }
 
-	}
+    /**.
+     * termine la session des log et ferme les flux
+     */
+    static synchronized void terminerSessionLog() {
+        FileWriter fw;
+        try {
+            fw = new FileWriter(path, true);
+            BufferedWriter bw;
+            bw = new BufferedWriter(fw);
+            PrintWriter fichier = new PrintWriter(bw);
+            fichier.println("\n\n");
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
 }
