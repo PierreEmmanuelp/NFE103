93595191ca35c423464d056a5d577ecfdbd5a505
Pierre-Emmanuel.Pour <Pierre-Emmanuel.Pour@HMVAFN83.suedwesteuropa.rheinzink.int>
2014-03-19 08:09:26 +0100

    modification commentaires javadoc

--- src/debug/Trace.java
+++ src/debug/Trace.java
@@ -1,17 +1,14 @@
 package debug;
-/**.
- * Permet d'écrire dans la sortie standard si la variable debug est a true;
+/** Permet d'écrire dans la sortie standard si la variable debug est a true.
  * @author Pourquier Pierre-Emmanuel
  * @version 1.0
  */
 public class Trace {
-    /**.
-     * Boolean debug : si le projet est en mode debug
+    /** Boolean debug : si le projet est en mode debug.
      */
     private static boolean debug = false;
 
-    /**.
-    * Ecrit la ligne dans la sortie standard si this.debug = true
+    /** Ecrit la ligne dans la sortie standard si this.debug = true.
     * @param strLigne ligne à écrire
     */
     public static void trace(final String strLigne) {
@@ -20,16 +17,14 @@ public class Trace {
         }
     }
 
-    /**.
-     * exprime si le projet est en debug
+    /** exprime si le projet est en debug.
      * @return true si le debug est actif
      */
     public static boolean isDebug() {
         return debug;
     }
 
-    /**.
-     * permet de spécifié si le debug ets actif.
+    /** permet de spécifié si le debug est actif.
      * @param pdebug true si le projet est en debug
      */
     public static void setDebug(final boolean pdebug) {
