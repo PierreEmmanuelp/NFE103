0beef925569b41caf3273513f703058a32d029cd
benjamin.pierrot@univ-st-etienne.fr <benjamin.pierrot@univ-st-etienne.fr>
2014-03-19 21:37:06 +0100

    Correction bug commit 06dbebe5d0bd39a155b8711541321daf7a589d07

--- src/http/Client.java
+++ src/http/Client.java
@@ -81,8 +81,7 @@ public class Client implements Runnable {
                 Http.syslog.error(msg);
             }
             try { //lecture dans le socket
-                line = in.readLine();
-                while (line != null && line.length() > 0) {
+                while ((line = in.readLine()) != null && line.length() > 0) {
                         header.add(line);
                     }
                 requete = new Request(header);
