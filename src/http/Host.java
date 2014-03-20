db515f4b326ed9d84eda1efab248be1b414c09f1
Pierre-Emmanuel.Pour <Pierre-Emmanuel.Pour@HMVAFN83.suedwesteuropa.rheinzink.int>
2014-03-20 08:47:48 +0100

    Redéfinition du toString()

--- src/http/Host.java
+++ src/http/Host.java
@@ -1,59 +1,65 @@
 package http;
 
 /**
- * représente un host sur le serveur http
- * @author Morgan Pinatel
- * @version 1.0
+ * représente un host sur le serveur http.
+ * @author Morgan Pinatel, Pourquier Pierre-Emmanuel
+ * @version 1.1
  */
 public class Host {
-    
+
     private String name;
     private String path;
-    
+
     /**
-    * contructeur de la classe. 
-    * @param pName 
+    * contructeur de la classe.
+    * @param pName
     * @param pPath
     */
     public Host(String pName, String pPath){
         this.name = pName;
         this.path = pPath;
     }
-    
+
     /**
-    * permet de récupérer le nom de l'hôte
+    * permet de récupérer le nom de l'hôte.
     * @return le nom de l'hôte
     */
-    public String getName() 
+    public String getName()
     {
         return name;
     }
-    
+
      /**
-    * permet de récupérer le chemin de l'hôte
+    * permet de récupérer le chemin de l'hôte.
     * @return le chemin de l'hôte
     */
-    public String getPath() 
+    public String getPath()
     {
         return path;
     }
-    
+
      /**
-    * permet de définir le nom de l'hôte
+    * permet de définir le nom de l'hôte.
     * @param pName
     */
     public void setName(String pName)
     {
         this.name=pName;
     }
-    
+
      /**
-    * permet de définir le chemin de l'hôte
+    * permet de définir le chemin de l'hôte.
     * @param pPath
     */
     public void setPath(String pPath)
     {
         this.path=pPath;
     }
-    
+
+    @Override
+    public final String toString() {
+        return name;
+    }
+
+
 }
